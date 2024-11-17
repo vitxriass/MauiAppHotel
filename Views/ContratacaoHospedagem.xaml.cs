88d174837d3377764fff29cb99435efb0b7dfb21
vitxriass <vitoriasilvasan23@gmail.com>
2024-11-16 23:50:04 -0300

    .

--- Views/ContratacaoHospedagem.xaml.cs
+++ Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -19,7 +21,7 @@ public partial class ContratacaoHospedagem : ContentPage
         dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
@@ -35,17 +37,27 @@ public partial class ContratacaoHospedagem : ContentPage
     {
         try
         {
-            
-            await Navigation.PushAsync(new Sobre());
+
+            Hospedagem h = new Hospedagem
+            {
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+                QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                DataCheckIn = dtpck_checkin.Date,
+                DataCheckOut = dtpck_checkout.Date,
+            };
+
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            });
+
         }
         catch (Exception ex)
         {
-            DisplayAlert("Ops", ex.Message, "OK");
+            await DisplayAlert("Ops", ex.Message, "OK");
         }
     }
-
-
-
 
     private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
     {
---
+++ Views/Hospedagem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MauiAppHotel.Views
+{
+    internal class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; internal set; }
+        public int QntAdultos { get; internal set; }
+        public int QntCriancas { get; internal set; }
+        public DateTime DataCheckIn { get; internal set; }
+        public DateTime DataCheckOut { get; internal set; }
+    }
+}
